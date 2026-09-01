09ab7d39b1647ff2ef567a2c6edd57fbad1f2bc5
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2026-09-01 07:57:25 -0400

    Add scroll event listener to change nav background

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+console.log("OUR-FAMILY LOADED");
 
+window.addEventListener("scroll", () => {
+
+let nav = document.querySelector("nav");
+
+if(window.scrollY > 50){
+nav.style.background = "#000";
+}
+else{
+nav.style.background = "#1b1b1b";
+}
+
+});
